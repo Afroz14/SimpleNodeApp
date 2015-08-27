09d5dc68a401771e0a10cb49c250fe38666a0321
afroz <afroz.alam@proptiger.com>
2015-08-27 16:29:57 +0530

    Login page added

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express  = require('express'),
     app      = express(),
-    config   = require('./src/config/config');
+    config   = require('./src/config/config'),
+    swig     = require('swig'),
+    router   = require('./src/controllers/index');
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.use('/',router);
+
+
+// template setting : We are using swig
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/src/views');
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+
 
 // start server
 app.listen(config.port, function() {
